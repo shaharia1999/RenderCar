b43cf0241954b1b39ae8c9193113ce809ab9066c
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-05-27 14:27:42 +0600

    api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const app=express()
-const stripe = require("stripe")(process.env.STRIPE_Key)
+const stripe = require("stripe")(process.env.SRIPE_KEY)
 var jwt = require('jsonwebtoken');
 require('dotenv').config();
 
